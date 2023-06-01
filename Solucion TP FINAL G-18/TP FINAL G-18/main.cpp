05486e73432be287cfbaaac2fa50e3d153cc0996
MartinaHanselmann <martuhanselmann@gmail.com>
2023-06-01 11:45:46 -0300

    cambios

---
+++ Solucion TP FINAL G-18/TP FINAL G-18/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <list>
+#include <iterator>
+
+#include "cANPA.h"
+#include "cAsociacion.h"
+#include "cPiezasOrtopedicas.h"
+#include "cPersona.h"
+
+int main()
+{
+	cAsociacion aso("Lola", "Aguilar 345");
+
+	cPersona("Florencia", "Aguirre", "1145566767");
+
+	cFabricantes fabri("FabriProtesis", "Aguilar 345", "2346-gh", false, "todo verificado");
+
+	cMedicos medic("Laura", "Gonzalez", "1188883333", "M.P.202345", true);
+
+	//cOrtopedias
+
+	//cHospitales hospi("Zabala", "Libertador 123", "Manuel", "Jorge");
+
+	//cPiezasOrtopedicas ortop(354, "Ceramica", time_t , cFabricantes, 150);
+
+	//cPacientes 
+
+	//cQuirurgicas
+
+	//cNoQuirurgicas
+
+	//cANPA
+
+
+
+
+
+
+}
