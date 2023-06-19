365f540bc9fb24c9755f45152b1d6e880edd7523
Nicolás Rapp <nicolas.rapp.ar@gmail.com>
2023-06-19 19:03:12 -0300

    mas cambios en el main

--- Solucion TP FINAL G-18/TP FINAL G-18/main.cpp
+++ Solucion TP FINAL G-18/TP FINAL G-18/main.cpp
@@ -26,17 +26,23 @@ int main()
 	tm FechaEstimEntre = { 0, 0, 0, 0, 0, 0 };
 	tm FechaEntre = { 0, 0, 0, 0, 0, 0 };
 
-	//DEFINIMOS ALGUNOS PACIENTES
+	//DEFINIMOS ALGUNOS PACIENTES ESTÁTICOS
 	cPacientes Nicolas("Nicolas", "Rapp", "45545166", FechaNac, SinAlergia, "Hospital Favaloro", 12, true, true);
 	cPacientes Martina("Martina", "Hanselmann", "00112233", FechaNac, Xenobiotico, "Hospital Favaloro", 22, false, true);
 	cPacientes Franco("Franco", "Sorella", "33445566", FechaNac, Liquenoide, "Hospital Favaloro", 9, true, false);
 	cPacientes MartinS("Martin", "Scorza", "11223344", FechaNac, Xenobiotico, "Hospital Favalororo", 20, false, true);
+
 	cPacientes Nadie("Nombre", "Apellido", "DNI", FechaNac, SinAlergia, "Hospital Asistente", 324, true, false);
 
 	cPacientes Camila("Camila", "Zavidowski", "99887766", FechaNac, SinAlergia, "Hospital Aleman", 2, true, true);
 	cPacientes Mariano("Mariano", "Bordeira", "77665544", FechaNac, Xenobiotico, "Hospital Aleman", 4, false, true);
 	cPacientes Stefan("Stefan", "Napoletano", "55443322", FechaNac, Liquenoide, "Hospital Aleman", 6, true, false);
 	cPacientes Emilia("Emilia", "Janin", "33221100", FechaNac, Xenobiotico, "Hospital Aleman", 8, false, true);
+
+	cPacientes Rodrigo("Rodrigo", "Zavidowski", "114023342", FechaNac, SinAlergia, "Hospital Italiano", 1, true, true);
+	cPacientes Federico("Federico", "Fioriti", "98877766", FechaNac, Xenobiotico, "Hospital Italiano", 3, true, false);
+	cPacientes Mateo("Mateo", "Espejo", "12233344", FechaNac, Liquenoide, "Hospital Italiano", 5, false, false);
+	cPacientes MariaD("Maria", "Durand", "45566677", FechaNac, Carcinogenicos, "Hospital Italiano", 7, false, true);
 
 	list <cPacientes> PacientesFavaloro; //AGREGAMOS A LA LISTA A LOS PACIENTES CREADOS
 	PacientesFavaloro.push_back(Nicolas);
@@ -50,8 +56,14 @@ int main()
 	PacientesAleman.push_back(Stefan);
 	PacientesAleman.push_back(Emilia);
 
-	//DEFINIMOS ALGUNOS MEDICOS
-	cMedicos Maria("Maria", "Gonzalez", "1188883333", "Matricula: M.P.202345", true);
+	list <cPacientes> PacientesItaliano; //AGREGAMOS A LA LISTA A LOS PACIENTES CREADOS
+	PacientesAleman.push_back(Rodrigo);
+	PacientesAleman.push_back(Federico);
+	PacientesAleman.push_back(Mateo);
+	PacientesAleman.push_back(MariaD);
+
+	//DEFINIMOS ALGUNOS MEDICOS ESTÁTICOS
+	cMedicos MariaG("Maria", "Gonzalez", "1188883333", "Matricula: M.P.202345", true);
 	cMedicos Manuel("Manuel", "Martinez", "1123457896", "Matricula: M.P.346754", false);
 	cMedicos Ana("Ana", "Gomez", "11345098", "Matricula: M.P.345689", true);
 	cMedicos Maximo("Maximo", "Rapp", "11459821", "Matricula: M.P.457293", false);
@@ -61,8 +73,13 @@ int main()
 	cMedicos Isabella("Isabella", "Ferrari", "98765", "Matricula: M.P.148126", true);
 	cMedicos MartinB("Martin", "Ballarino", "43210", "Matricula: M.P.78216", false);
 
+	cMedicos Laura("Laura", "Gonzalez", "1188883333", "Matricula: M.P.202345", true);
+	cMedicos Renata("Renata", "Stasi", "7853225", "Matricula: M.P.782652", false);
+	cMedicos Marcos("Marcos", "Jurza", "4768135", "Matricula: M.P.5482235", true);
+	cMedicos Sol("Sol", "Fitterman", "4545246", "Matricula: M.P.1542359", false);
+
 	list <cMedicos> MedicosFavaloro;
-	MedicosFavaloro.push_back(Maria);
+	MedicosFavaloro.push_back(MariaG);
 	MedicosFavaloro.push_back(Manuel);
 	MedicosFavaloro.push_back(Ana);
 	MedicosFavaloro.push_back(Maximo);
@@ -73,16 +90,48 @@ int main()
 	MedicosAleman.push_back(Isabella);
 	MedicosAleman.push_back(MartinB);
 
-	//DEFINIMOS ALGUNOS HOSPITALES
+	list <cMedicos> MedicosItaliano;
+	MedicosItaliano.push_back(Laura);
+	MedicosItaliano.push_back(Renata);
+	MedicosItaliano.push_back(Marcos);
+	MedicosItaliano.push_back(Sol);
+
+	//DEFINIMOS ALGUNOS HOSPITALES ESTÁTICOS
 	list <cHospitales> Hospitales;
 
 	cHospitales Favaloro("Hospital Favaloro", "Libertador 123", MedicosFavaloro, PacientesFavaloro);
 	cHospitales Aleman("Hospital Aleman", "Olleros 4567", MedicosAleman, PacientesAleman);
+	cHospitales Italiano("Hospital Italiano", "Libertador 123", MedicosItaliano, PacientesItaliano);
 
 	Hospitales.push_back(Favaloro);
 	Hospitales.push_back(Aleman);
+	Hospitales.push_back(Italiano);
 
 //RESTO	
+	//DEFINIMOS ALGUNOS PACIENTES DINÁMICOS
+	cPacientes* Rodrig = new cPacientes("Rodrigo", "Zavidowski", "114023342", FechaNac, SinAlergia, "Hospital Italiano", 1, true, true);
+	cPacientes* Federic = new cPacientes("Federico", "Fioriti", "98877766", FechaNac, Xenobiotico, "Hospital Italiano", 3, true, false);
+	cPacientes* Mate = new cPacientes("Mateo", "Espejo", "12233344", FechaNac, Liquenoide, "Hospital Italiano", 5, false, false);
+	cPacientes* Mari = new cPacientes("Maria", "Durand", "45566677", FechaNac, Carcinogenicos, "Hospital Italiano", 7, false, true);
+
+	//DEFINIMOS ALGUNOS MEDICOS DINÁMICOS
+	cMedicos* Laurit = new cMedicos("Laura", "Gonzalez", "1188883333", "Matricula: M.P.202345", true);
+	cMedicos* Renat = new cMedicos("Renata", "Stasi", "7853225", "Matricula: M.P.782652", false);
+	cMedicos* Marco = new cMedicos("Marcos", "Jurza", "4768135", "Matricula: M.P.5482235", true);
+	cMedicos* So = new cMedicos("Sol", "Fitterman", "4545246", "Matricula: M.P.1542359", false);
+
+	//DEFINIMOS UN HOSPITAL DINÁMICO
+	cHospitales* Italian = new cHospitales("Hospital Italiano", "Libertador 123", MedicosItaliano, PacientesItaliano);
+
+	//DEFINIMOS ALGUNAS FABRICAS DINÁMICAS
+	cFabricantes* Fabrica = new cFabricantes("Fabri Protesis", "Aguilar 345", "Habilitado", false, "Requisitos: todo confirmado");
+
+	//DEFINIMOS ALGUNAS PIEZAS ORTOPEDICAS DINÁMICAS
+	cPiezasOrtopedicas* PiezitaOrtoped = new cPiezasOrtopedicas(354, Ceramica, FechaFabric, Fabrica, SuperiorDerecho, "XQRW456");
+
+	//DEFINIMOS ALGUNOS REGISTROS ESTÁTICOS
+	cRegistros Registro2(Italian, Laurit, FechaSolic, FechaEstimEntre, FechaEntre, PiezitaOrtoped, Rodrig);
+
 	list <cOrtopedias> Ortopedias;
 	list <cPiezasOrtopedicas> PiezitasOrtopedicas;
 	list <cRegistros> Register;
@@ -90,14 +139,8 @@ int main()
 
 	//no hace falta simular las clases cAsociacion ni cPersona
 	//Simulacion
-	cFabricantes* Fabrica = new cFabricantes("Fabri Protesis", "Aguilar 345", "Habilitado", false, "Requisitos: todo confirmado");
 	cFabricantes Fabrica2 ("Fabri Protesis", "Aguilar 345", "Habilitado", false, "Requisitos: todo confirmado");
-	cMedicos* Laurita = new cMedicos("Laura", "Gonzalez", "1188883333", "Matricula: M.P.202345", true);
-	cHospitales* Hospi = new cHospitales("Hospital Italiano", "Libertador 123", MedicosFavaloro, PacientesFavaloro);
-	cPiezasOrtopedicas* PiezitaOrtoped = new cPiezasOrtopedicas(354, Ceramica, FechaFabric, Fabrica, SuperiorDerecho, "XQRW456");
-	cPacientes* Rodrigo = new cPacientes("Rodrigo", "Zavidowski", "114023342", FechaNac, SinAlergia, "Hospital Italiano", 324, true, false);
-	cRegistros* Registro = new cRegistros(Hospi, Laurita, FechaSolic, FechaEstimEntre, FechaEntre, PiezitaOrtoped, Rodrigo);
-	cRegistros Registro2 (Hospi, Laurita, FechaSolic, FechaEstimEntre, FechaEntre, PiezitaOrtoped, Rodrigo);
+	//cRegistros* Registro = new cRegistros(Italian, Laurit, FechaSolic, FechaEstimEntre, FechaEntre, PiezitaOrtoped, Rodrig);
 	cANPA PAMPA(Register, "Solicitud Especial", Ortopedias, Hospitales);
 	cOrtopedias Ortoped("Ortopedita", "Manuel Ugarte 354", PiezitasOrtopedicas, 3456);
 	cQuirurgicas Quirur(25, Cobalto, FechaFabric, Fabrica, InferiorDerecha, "ZZ1234");
@@ -181,6 +224,7 @@ int main()
 			break;
 
 		case(3):
+			//SALTA ERROR DE DEBUG EN EL BUSCAR POR HOSPI
 			system("cls");
 
 			cout << "\tIngrese el nombre de su hospital: ";
